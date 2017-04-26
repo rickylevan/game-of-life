469ea9ba140d30be5575f2f27ef3a4526a1af33e
Ricky LeVan <rick.levan@gmail.com>
2017-04-26 11:23:42 -0500

    squares up

--- app/index.js
+++ app/index.js
@@ -5,17 +5,48 @@ require('./index.css');
 
 class App extends React.Component {
   render() {
+    var words = ["hey", "ricky", "you", "so", "fine"];
     return (
-      <Romeo romeoText="hi ricky, you so fine. fo sho"/>
+      <div>
+      <Romeo romeoText={words}/>
+      <table>
+<tr><td/><td/><td/></tr>
+<tr><td/><C3 msg='boobs'/><td/></tr>
+<tr><C3 msg='tits' /><C3 msg='boobies'/><C3 msg='ass'/></tr>
+</table>
+      </div>
     )
   }
 }
 
 
+
+class C3 extends React.Component {
+  render() {
+    if (this.props.msg === "boobs") {
+      console.log('found boobs!');
+      return <td style={{"backgroundColor": "#ff0000"}} />
+    } else {
+      return <td 
+        onClick={() => console.log(this.props.msg)}
+        style={{"backgroundColor": "#0000ff"}}
+      />
+    }
+  }
+}
+
+function wordMap(words) {
+  return words.map(function(word) {
+    return (
+      <div> {word} </div>
+    )
+  })
+}
+
 class Romeo extends React.Component {
   render() {
     return (
-      <div> {this.props.romeoText} </div>
+      wordMap(this.props.romeoText)
     )
   }
 }
@@ -26,6 +57,4 @@ ReactDOM.render(
 );
 
 
-Romeo.propTypes = {
-  romeoText: PropTypes.string.isRequired
-}
+
