bc0573f50f980bc59bb289d366eac082a5c54b32
Ricky LeVan <rick.levan@gmail.com>
2017-04-25 19:58:24 -0500

    custom component, but proptypes not working

--- app/index.js
+++ app/index.js
@@ -1,11 +1,21 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
 require('./index.css');
 
 class App extends React.Component {
   render() {
     return (
-      <div>Hello World!</div>
+      <Romeo romeoText="hi ricky, you so fine. fo sho"/>
+    )
+  }
+}
+
+
+class Romeo extends React.Component {
+  render() {
+    return (
+      <div> {this.props.romeoText} </div>
     )
   }
 }
@@ -14,3 +24,8 @@ ReactDOM.render(
   <App />,
   document.getElementById('app')
 );
+
+
+Romeo.propTypes = {
+  romeoText: PropTypes.string.isRequired
+}
