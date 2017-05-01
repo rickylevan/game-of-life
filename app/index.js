416fdbefb890274cf456f790798d838d9e72d634
Ricky LeVan <rick.levan@gmail.com>
2017-05-01 15:48:45 -0500

    less fucked state, map still wonky

--- app/index.js
+++ app/index.js
@@ -3,11 +3,22 @@ var ReactDOM = require('react-dom');
 var PropTypes = require('prop-types');
 require('./index.css');
 
+                /*return <Bit 
+                  val={this.state.vals[idx][idy]}
+                  f={this.toggle.bind(this).bind(null, idx, idy)}
+                />*/
+
+
+
 var N = 5;
 class App extends React.Component {
   constructor() {
     super();
-    this.state.vals = new Array(N).fill(new Array(N).fill(true));
+    this.state = {
+      vals: new Array(N).fill(new Array(N).fill(true))
+    }
+
+    console.log(this.state.vals);
   }
 
   toggle(idx, idy) {
@@ -18,7 +29,18 @@ class App extends React.Component {
 
   render() {
     return (
-      <h1> Hi! </h1>
+      <table>
+        {this.state.vals.map((val, idx) => {
+            <tr>
+              {val.map((_, idy) => {
+                <Bit 
+                  val={this.state.vals[idx][idy]}
+                  f={this.toggle.bind(this).bind(null, idx, idy)}
+                />
+              })}
+            </tr>
+        })}
+      </table>
     )
   }
 }
@@ -28,8 +50,8 @@ function Bit(props) {
   var colorFalse = "#000000";
 
   return (
-    <div 
-      style={{"backgroundColor": this.props.val ? colorTrue : colorFalse}}
+    <td 
+      style={{"backgroundColor": props.val ? colorTrue : colorFalse}}
       onClick={props.f}
     />
   )
